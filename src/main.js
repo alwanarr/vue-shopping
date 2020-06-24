bce6d3d1c24707cdf3f02db8e7d8520e98b41dcb
Alwan-ar <muhammadalwanarfianto2013@gmail.com>
2020-06-24 11:11:46 +0700

    deploy to master

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ import {store} from './store';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import VueSweetalert2 from 'vue-sweetalert2';
 
-Vue.use({
-  // this is the required "install" method for Vue plugins
-  install (Vue) {
-    Vue.swal = VueSweetalert2
-    Vue.prototype.$swal = VueSweetalert2
-  }
-})
+// Vue.use({
+//   // this is the required "install" method for Vue plugins
+//   install (Vue) {
+//     Vue.swal = VueSweetalert2
+//     Vue.prototype.$swal = VueSweetalert2
+//   }
+// })
 
 Vue.use(VueSweetalert2);
 new Vue({
